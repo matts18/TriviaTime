5a799cd33fed68fe4d47230cc4681e2d93ad445f
matts18 <snelgarm@gmail.com>
2021-09-08 07:12:47 +0800

    add swagger

--- TriviaTime/Startup.cs
+++ TriviaTime/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace TriviaTime
 {
@@ -27,6 +28,10 @@ namespace TriviaTime
         {
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Trivia Time", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +40,8 @@ namespace TriviaTime
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TriviaTimea v1"));
             }
 
             app.UseHttpsRedirection();
